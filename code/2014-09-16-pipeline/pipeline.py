74f098cf7b647de4430a8ee09145910bb856d74e
Berk Geveci <berk.geveci@kitware.com>
2014-10-05 17:22:18 -0400

    Another blog

--- code/2014-09-16-pipeline/pipeline.py
+++ code/2014-09-16-pipeline/pipeline.py
@@ -35,6 +35,15 @@ class MyFilter(VTKPythonAlgorithmBase):
     def RequestInformation(self, request, inInfo, outInfo):
         print "MyFilter RequestInformation:"
         print outInfo.GetInformationObject(0)
+        metaData = inInfo[0].GetInformationObject(0).Get(
+            metaDataKey)
+        newMetaData = metaData.NewInstance()
+        newMetaData.ShallowCopy(metaData)
+        someArray = vtk.vtkCharArray()
+        someArray.SetName("someArray")
+        newMetaData.GetFieldData().AddArray(someArray)
+        outInfo.GetInformationObject(0).Set(metaDataKey, newMetaData)
+        print outInfo.GetInformationObject(0)
         return 1
 
     def RequestUpdateExtent(self, request, inInfo, outInfo):
@@ -52,4 +61,4 @@ s = MySource()
 f = MyFilter()
 f.SetInputConnection(s.GetOutputPort())
 
-f.Update()
+f.UpdateInformation()
