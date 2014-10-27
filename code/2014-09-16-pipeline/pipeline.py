92a17d26d48ae0de81ca09f2df622c3bcf767258
Berk Geveci <berk.geveci@kitware.com>
2014-10-27 19:46:52 -0400

    Another post

--- code/2014-09-16-pipeline/pipeline.py
+++ code/2014-09-16-pipeline/pipeline.py
@@ -3,6 +3,7 @@ from vtk.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from vtk.util import keys
 
 metaDataKey = keys.MakeKey(keys.DataObjectMetaDataKey, "a meta-data", "my module")
+requestKey = keys.MakeKey(keys.IntegerRequestKey, "a request", "my module")
 
 class MySource(VTKPythonAlgorithmBase):
     def __init__(self):
@@ -18,12 +19,19 @@ class MySource(VTKPythonAlgorithmBase):
 
     def RequestUpdateExtent(self, request, inInfo, outInfo):
         print "MySource RequestUpdateExtent:"
-#        print outInfo.GetInformationObject(0)
+        print outInfo.GetInformationObject(0)
         return 1
 
     def RequestData(self, request, inInfo, outInfo):
         print "MySource RequestData:"
-#        print outInfo.GetInformationObject(0)
+        outInfo0 = outInfo.GetInformationObject(0)
+        areq = outInfo0.Get(requestKey)
+        s = vtk.vtkSphereSource()
+        s.SetRadius(areq)
+        s.Update()
+        output = outInfo0.Get(vtk.vtkDataObject.DATA_OBJECT())
+        output.ShallowCopy(s.GetOutput())
+        print output
         return 1
 
 class MyFilter(VTKPythonAlgorithmBase):
@@ -48,12 +56,22 @@ class MyFilter(VTKPythonAlgorithmBase):
 
     def RequestUpdateExtent(self, request, inInfo, outInfo):
         print "MyFilter RequestUpdateExtent:"
-#        print outInfo.GetInformationObject(0)
+        print outInfo.GetInformationObject(0)
+        areq = outInfo.GetInformationObject(0).Get(requestKey)
+        inInfo[0].GetInformationObject(0).Set(requestKey, areq + 1)
         return 1
 
     def RequestData(self, request, inInfo, outInfo):
         print "MyFilter RequestData:"
-#        print outInfo.GetInformationObject(0)
+        inInfo0 = inInfo[0].GetInformationObject(0)
+        outInfo0 = outInfo.GetInformationObject(0)
+        input = inInfo0.Get(vtk.vtkDataObject.DATA_OBJECT())
+        output = outInfo0.Get(vtk.vtkDataObject.DATA_OBJECT())
+        sh = vtk.vtkShrinkPolyData()
+        sh.SetInputData(input)
+        sh.Update()
+        output.ShallowCopy(sh.GetOutput())
+        print output
         return 1
 
 s = MySource()
@@ -62,3 +80,6 @@ f = MyFilter()
 f.SetInputConnection(s.GetOutputPort())
 
 f.UpdateInformation()
+outInfo = f.GetOutputInformation(0)
+outInfo.Set(requestKey, 0)
+f.Update()
